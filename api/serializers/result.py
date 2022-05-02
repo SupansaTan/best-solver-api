980b94edac2b55d073d768f72d0d703513e034de
hathaipat-p <hathaipat44@hotmail.co.th>
2022-05-02 17:25:59 +0700

    add data in serializers, views _init_

--- api/serializers/result.py
+++ api/serializers/result.py
@@ -3,3 +3,4 @@ from rest_framework import serializers
 class ResultSerializer(serializers.Serializer):
   result = serializers.FloatField()
   time_used = serializers.FloatField()
+  graph = serializers.CharField()
--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,1 +1,5 @@
 from api.views.root import *
+from api.views.bisection import *
+from api.views.falsi import *
+from api.views.newton import *
+from api.views.secant import *
