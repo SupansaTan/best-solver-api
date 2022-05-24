82585bd17a28456844a4cba749a3d4d803d750dd
hathaipat-p <hathaipat44@hotmail.co.th>
2022-05-24 21:11:27 +0700

    change path similar to React

--- api/urls.py
+++ api/urls.py
@@ -8,11 +8,11 @@ schema_view = get_swagger_view(title='Best Solver API')
 urlpatterns = [
   path('', schema_view, name='swagger-ui'),  # still error because don't have any api
   path('root', views.Root.as_view(), name='root-finding'),
-  path('bisection/<int:id>', views.Bisection.as_view(), name='root-bisection'),
-  path('falsi/<int:id>', views.Falsi.as_view(), name='root-falsi'),
-  path('newton/<int:id>', views.Newton.as_view(), name='root-newton'),
-  path('secant/<int:id>', views.Secant.as_view(), name='root-secant'),
-  path('reimann/<int:id>', views.Reimann.as_view(), name='integral-reimann'),
-  path('trapezoid/<int:id>', views.Trapezoid.as_view(), name='integral-trapezoid'),
-  path('simpson/<int:id>', views.Simpson.as_view(), name='integral-simpson'),
+  path('Bisection/<int:id>', views.Bisection.as_view(), name='root-bisection'),
+  path('RegulaFalsi/<int:id>', views.Falsi.as_view(), name='root-falsi'),
+  path('Newton/<int:id>', views.Newton.as_view(), name='root-newton'),
+  path('Secant/<int:id>', views.Secant.as_view(), name='root-secant'),
+  path('RiemannSum/<int:id>', views.Reimann.as_view(), name='integral-reimann'),
+  path('TrapezoidRule/<int:id>', views.Trapezoid.as_view(), name='integral-trapezoid'),
+  path('SimsonRule/<int:id>', views.Simpson.as_view(), name='integral-simpson'),
 ]
